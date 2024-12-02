a773ec280ae0d62b5a8d7cdb91f121e0ac536754
shahid <shahidshah77562@gmail.com>
2024-12-02 14:09:19 +0530

    Add files via upload

---
+++ ashraf1.java
@@ -0,0 +1,10 @@
+import java.io.PrintWriter;
+public class ashraf1{
+    public static void main(String args[]){
+        PrintWriter Pw=new PrintWriter(System.out);
+        Pw.println("hello world");
+        Pw.print("ashraf");
+        Pw.print("print statement");
+        Pw.close();
+    }
+}

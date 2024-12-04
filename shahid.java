400c7b16c7eb1e91f580de3f208565e78aff62a3
shahid <shahidshah77562@gmail.com>
2024-12-04 21:29:47 +0530

    Add files via upload

---
+++ shahid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class shahid {
+    public static void main(String[]args ){
+        //conversion
+
+       
+        char c='a';
+        int b=c;
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+
+      
+    }
+
+    
+}

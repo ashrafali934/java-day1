2ab57121b0450bb74ed4cade2c29bcab04137b3a
shahid <shahidshah77562@gmail.com>
2024-12-04 21:28:41 +0530

    Add files via upload

---
+++ shahid1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class shahid1 {
+    public static void main(String[] args) {
+        //Accepting a String
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int b=sc.nextInt(); 
+        String c=String.format("the name of person is %s and his age is %d",a,b);
+        System.out.println(c);
+    }
+}
+        

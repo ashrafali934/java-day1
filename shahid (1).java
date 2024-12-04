826ce7564bb2a137945f3237cad50ef6812771da
shahid <shahidshah77562@gmail.com>
2024-12-04 21:29:19 +0530

    Add files via upload

---
+++ shahid (1).java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class shahid {
+    public static void main(String[] args) {
+        //Accepting a String
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=String.format("congrajulations %s you won 5 lacks",a);
+        System.out.println(b);
+    }
+}
+        
